7f54a632c66804482a67814be0d96f3398aab83c
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-26 21:55:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -84,9 +84,8 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    var message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     cart = []
-    return message
   }
 }
 
